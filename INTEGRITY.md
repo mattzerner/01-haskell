1363e53f3328a6a7078cde2401ea8f114a9e8ee4
unknown <mzerner@ucsc.edu>
2026-02-01 01:40:14 -0800

    Restaged branch

---
+++ INTEGRITY.md
@@ -0,0 +1,4 @@
+I incorporated elements from the following sources into my assignment submission.
+
+* Lecture recordings on YuJa
+* Section recordings in YuJa (Thurs. 7 pm)
---
+++ src/Hw1.hs
@@ -0,0 +1,144 @@
+{- | CSE 114A: Intro to Haskell Assignment.
+     Do not change the skeleton code!
+
+     You may only replace the `error "TBD:..."` parts.
+
+     For this assignment, you may use any library function on integers
+     but only the following library functions on lists:
+
+     (++)
+     (==)
+
+ -}
+
+module Hw1 where
+
+import Prelude  hiding (replicate, sum, reverse)
+
+
+-- | Reverse the order of elements in a list; in other words,
+--   `listReverse [x1, x2, ..., xn]` returns `[xn, ..., x2, x1]`.
+--
+-- >>> listReverse [1,2,3,4]
+-- [4,3,2,1]
+--
+-- >>> listReverse ["i", "want", "to", "ride", "my", "bicycle"]
+-- ["bicycle", "my", "ride", "to", "want", "i"]
+--
+-- >>> listReverse []
+-- []
+
+listReverse :: [a] -> [a]
+listReverse xs = rev xs []
+  where
+    rev []     acc = acc
+    rev (y:ys) acc = rev ys (y:acc)
+
+
+-- | Determine whether a string is a palindrome (i.e. spelled the same
+--   both backwards and forwards).
+--
+-- >>> palindrome "malayalam"
+-- True
+--
+-- >>> palindrome "palindrome"
+-- False
+--
+-- >>> palindrome ""
+-- True
+
+palindrome :: String -> Bool
+palindrome w = w == listReverse w
+
+
+-- | `digitsOfInt n` should return `[]` if `n` is not positive,
+--    and otherwise returns the list of digits of `n` in the
+--    order in which they appear in `n`.
+--
+-- >>> digitsOfInt 3124
+-- [3, 1, 2, 4]
+--
+-- >>> digitsOfInt 352663
+-- [3, 5, 2, 6, 6, 3]
+--
+-- >>> digitsOfInt -42
+-- []
+
+digitsOfInt :: Integer -> [Integer]
+digitsOfInt n =
+  if n <= 0 then []
+  else if n < 10 then [n]
+  else digitsOfInt (n `div` 10) ++ [n `mod` 10]
+
+
+-- | `digitsOfInts xs` should return a list containing all of the digits
+--   for every number in `xs`, in the same order they appeared in `xs`.
+--
+-- >>> digitsOfInts [3124, 52, -42, 8]
+-- [3, 1, 2, 4, 5, 2, 8]
+--
+-- >>> digitsOfInts []
+-- []
+
+digitsOfInts :: [Integer] -> [Integer]
+digitsOfInts xs =
+  if xs == [] then []
+  else digitsOfInt (head xs) ++ digitsOfInts (tail xs)
+
+
+-- | Doubles every other integer in a list,
+--   starting with the second element.
+--
+-- >>> doubleEveryOther [8,7,6,5]
+-- [8,14,6,10]
+--
+-- >>> doubleEveryOther [1,2,3]
+-- [1,4,3]
+--
+-- >>> doubleEveryOther []
+-- []
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther xs =
+  let go ys dbl =
+        if ys == [] then []
+        else
+          let x  = head ys
+              xs' = tail ys
+          in (if dbl then 2 * x else x) : go xs' (not dbl)
+  in go xs False
+
+
+-- | Sum the elements of a list
+--
+-- >>> sumList [1, 2, 3, 4]
+-- 10
+--
+-- >>> sumList [1, -2, 3, 5]
+-- 7
+--
+-- >>> sumList [1, 3, 5, 7, 9, 11]
+-- 36
+
+sumList :: [Integer] -> Integer
+sumList xs =
+  if xs == [] then 0
+  else head xs + sumList (tail xs)
+
+
+-- | Validate a credit card number
+--
+-- >>> validateCardNumber 4012888888881881
+-- True
+--
+-- >>> validateCardNumber 4012888888881882
+-- False
+
+validateCardNumber :: Integer -> Bool
+validateCardNumber =
+  \n ->
+    (sumList
+      (digitsOfInts
+        (doubleEveryOther
+          (listReverse (digitsOfInt n)))))
+    `mod` 10 == 0
